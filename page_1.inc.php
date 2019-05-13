08038f1a5ffe8e03293f3f2a5b4bdc525873bebf
Beda_IGIRANEZA <beda.igiraneza@gmail.com>
2019-05-13 20:50:18 +0200

    Ajout de fonction qui gerer les emails d'accuse de réception

--- page_1.inc.php
+++ page_1.inc.php
@@ -27,7 +27,29 @@
 				<td>
 					<input type="text" Sujet="votre demande" maxlength="500" required /><br />
 				</td>
-
+				<?php
+				function sanitize_my_email($field) {
+				    $field = filter_var($field, FILTER_SANITIZE_EMAIL);
+				    if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+				        return true;
+				    } else {
+				        return false;
+				    }
+						}
+		/*Envoyer un e-mail d'acuse de reception */
+				$to_email = '$nom @ etu.univ-smb.fr';
+				$subject = 'Acuse de reception de votre demande';
+				$message = "Merci pour l'interet que vous porte à votre formation, votre demande à été prise en compte. nous vous repondrons la blef delai \n Cordialement "";
+				$headers = 'From: noreply @ sevice de scolarite.fr';
+				//check if the email address is invalid $secure_check
+				$secure_check = sanitize_my_email($to_email);
+				if ($secure_check == false) {
+				    echo "Invalid input";
+				} else { //send email
+				    mail($to_email, $subject, $message, $headers);
+				    echo "This email is sent using PHP Mail";
+				}
+				?>
 				</td>
 			</tr>
 			<tr>
--- prof_page=4.php
+++ prof_page=4.php
@@ -42,7 +42,32 @@
 					<input type="hidden" name="checkForm" value="transmet les Message aux service technique">
 					<input type="submit" value="Envoyer la demande aux service technique">
 				</td>
+
+
 			</tr>
 		</table>
+		<?php
+		function sanitize_my_email($field) {
+				$field = filter_var($field, FILTER_SANITIZE_EMAIL);
+				if (filter_var($field, FILTER_VALIDATE_EMAIL)) {
+						return true;
+				} else {
+						return false;
+				}
+				}
+/*Envoyer un e-mail d'acuse de reception */
+		$to_email = '$nom @univ-smb.fr';
+		$subject = 'Acuse de reception de votre demande';
+		$message = "Votre demande à été prise en compte. \n Nous vous repondrons dans la blef delai \n Cordialement ";
+		$headers = 'From: noreply @ sevice de scolarite.fr';
+		//check if the email address is invalid $secure_check
+		$secure_check = sanitize_my_email($to_email);
+		if ($secure_check == false) {
+				echo "Invalid input";
+		} else { //send email
+				mail($to_email, $subject, $message, $headers);
+				echo "This email is sent using PHP Mail";
+		}
+		?>
 	</p>
 </form>
