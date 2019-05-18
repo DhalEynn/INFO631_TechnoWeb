63e6a5dc75001bab14927a22fc6cd80eb81fe2fb
aspalrobin <robin@colliaux-bernardes.net>
2019-05-18 13:03:23 +0200

    meilleur presentation

--- page_4.inc.php
+++ page_4.inc.php
@@ -1,11 +1,14 @@
 <?php
-$conn = connexion();
-	
-	if($_SESSION["travail"] = "Etudiant")
+	$conn = connexion();
+	if(isset($_POST["checkForm"]))
 	{
-		$sql = $conn->prepare ("SELECT * FROM `Demandes` where mailEtu = ?");
-		$sql->execute(array($_SESSION["mail"]));
-		while($array = $sql->fetch(PDO::FETCH_ASSOC))
+		unset($_POST["checkForm"]);
+		// Prepare the query.
+		$sql = $conn->prepare ("SELECT * FROM `demandes` where idDem = ?");
+		// Execute the query.
+		$sql->execute(array($_POST["idDem"]));
+			
+		if($array = $sql->fetch(PDO::FETCH_ASSOC))
 		{
 			echo "id : ";
 			echo $array["idDem"];
@@ -16,6 +19,9 @@ $conn = connexion();
 			echo "contenu : ";
 			echo $array["contenu"];
 			echo "<br/>";
+			echo "mail Etudiant : ";
+			echo $array["mailEtu"];
+			echo "<br/>";
 			echo "mail Professeur : ";
 			echo $array["mailProf"];
 			echo "<br/>";
@@ -25,14 +31,46 @@ $conn = connexion();
 			echo "<br/>";
 		}
 	}
-	elseif($_SESSION["travail"] = "Professeur")
-	{
-		$sql = $conn->prepare ("SELECT * FROM `Demandes` where mailProf = ?");
-		$sql->execute(array($_SESSION["mail"]));	
-	}
 	else
 	{
-		$sql = $conn->prepare ("SELECT * FROM `Demandes`");	
-		$sql->execute(array());
+		if(isEtudiant())
+		{
+			$sql = $conn->prepare ("SELECT * FROM `Demandes` where mailEtu = ?");
+			$sql->execute(array($_SESSION["mail"]));	
+		}	
+		elseif(isProfesseur())
+		{
+			$sql = $conn->prepare ("SELECT * FROM `Demandes` where mailProf = ?");
+			$sql->execute(array($_SESSION["mail"]));	
+		}
+		else
+		{
+			$sql = $conn->prepare ("SELECT * FROM `Demandes`");	
+			$sql->execute(array());
+		}
+			while($array = $sql->fetch(PDO::FETCH_ASSOC))
+			{
+				$id=$array["idDem"];
+				?>
+				<table>
+					<tr>
+						<td>
+							id : 
+						</td>	
+						<td>
+							<form method="post" action="projet.php?page=4">
+							<input type="hidden" name="checkForm" value="formulaire" />
+							<input type="submit" name="idDem" value="<?php echo $id?>" />
+						</td>
+					</tr>
+				</table>
+
+				<?php
+				//echo $array["idDem"];
+				echo "<br/>";
+				echo "sujet : ";
+				echo $array["sujet"];
+				echo "<br/>";
+			}
 	}	
 ?>
