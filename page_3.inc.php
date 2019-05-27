a88f359caffd2d30e948bb66651546743e594296
DahlEynn <addpro.mr@gmail.com>
2019-05-27 15:28:45 +0200

    Indentation

--- page_3.inc.php
+++ page_3.inc.php
@@ -1,133 +1,133 @@
 <?php
-if (isST())
-{
-	$conn = connexion();
-	if(isset($_POST["action"]))
+	if (isST())
 	{
-		unset($_POST["action"]);
-		if (isset($_POST["validation"]))
+		$conn = connexion();
+		if(isset($_POST["action"]))
 		{
-			if($_POST["validation"]== "Valider")
+			unset($_POST["action"]);
+			if (isset($_POST["validation"]))
 			{
-				updateStatus($_POST["valeurid"], "Valide");
+				if($_POST["validation"]== "Valider")
+				{
+					updateStatus($_POST["valeurid"], "Valide");
+				}
+				else
+				{
+					updateStatus($_POST["valeurid"], "Modifier");
+				}
+			}
+			header("refresh:0;url=projet.php?page=3");
+			die(0);
+		}
+		elseif(isset($_POST["checkForm"]))
+		{
+			unset($_POST["checkForm"]);
+			// Prepare the query.
+			$sql = $conn->prepare ("SELECT * FROM `demandes` WHERE idDem = ?");
+			// Execute the query.
+			$sql->execute(array($_POST["idDem"]));
+
+			if($array = $sql->fetch(PDO::FETCH_ASSOC))
+			{
+				echo "id : ";
+				echo $array["idDem"];
+				echo "<br/>";
+				echo "sujet : ";
+				echo $array["sujet"];
+				echo "<br/>";
+				echo "contenu : ";
+				echo $array["contenu"];
+				echo "<br/>";
+				echo "mail Etudiant : ";
+				echo $array["mailEtu"];
+				echo "<br/>";
+				echo "mail Professeur : ";
+				echo $array["mailProf"];
+				echo "<br/>";
+				echo "<br/>";
+				?>
+				<form method="post" action="projet.php?page=3">
+					<input type="hidden" name="action" value="formulaire" /></br>
+					<input type="submit" name="validation" value="Valider" />
+					<input type="hidden" name="valeurid" value="<?php echo $array["idDem"] ?>" required />
+					<input type="submit" name="validation" value="Renvoyer en modification" />
+				</form>
+				<?php
+				echo "<a href=\"?page=3\">Retour</a>";
+			}
+		}
+		else
+		{
+			$sql = $conn->prepare ("SELECT idDem, sujet, mailProf as mailPers, status FROM `demandes` WHERE status = 'Envoyee'");
+			$sql->execute(array());
+
+
+			if (is_null($sql) || $sql->rowCount() == 0)
+			{
+				echo "</br>Vous n'avez aucune demande en cours.";
 			}
 			else
 			{
-				updateStatus($_POST["valeurid"], "Modifier");
-			}
-		}
-		header("refresh:0;url=projet.php?page=3");
-		die(0);
-	}
-	elseif(isset($_POST["checkForm"]))
-	{
-		unset($_POST["checkForm"]);
-		// Prepare the query.
-		$sql = $conn->prepare ("SELECT * FROM `demandes` WHERE idDem = ?");
-		// Execute the query.
-		$sql->execute(array($_POST["idDem"]));
-
-		if($array = $sql->fetch(PDO::FETCH_ASSOC))
-		{
-			echo "id : ";
-			echo $array["idDem"];
-			echo "<br/>";
-			echo "sujet : ";
-			echo $array["sujet"];
-			echo "<br/>";
-			echo "contenu : ";
-			echo $array["contenu"];
-			echo "<br/>";
-			echo "mail Etudiant : ";
-			echo $array["mailEtu"];
-			echo "<br/>";
-			echo "mail Professeur : ";
-			echo $array["mailProf"];
-			echo "<br/>";
-			echo "<br/>";
 			?>
-			<form method="post" action="projet.php?page=3">
-				<input type="hidden" name="action" value="formulaire" /></br>
-				<input type="submit" name="validation" value="Valider" />
-				<input type="hidden" name="valeurid" value="<?php echo $array["idDem"] ?>" required />
-				<input type="submit" name="validation" value="Renvoyer en modification" />
-			</form>
+			<table>
+				<tr class="presentation">
+					<td class="idDem">
+						<center>id :</center>
+					</td>
+					<td class="sujetDem">
+						<center>Sujet :</center>
+					</td>
+					<td class="mailDem">
+						<center>
+							<?php
+								if (isProfesseur())
+								{
+									echo "Etudiant :";
+								}
+								else
+								{
+									echo "Référent :";
+								}
+							?>
+						</center>
+					</td>
+				</tr>
+				<!-- Ecarte le nom des colonnes du contenu des colonnes resultat -->
+				<tr class="ecarteColonne"></tr>
+				<form method="post" action="projet.php?page=3">
+					<input type="hidden" name="checkForm" value="formulaire" /></br>
+					<?php
+					while($array = $sql->fetch(PDO::FETCH_ASSOC))
+					{
+						?>
+						<tr>
+							<td class="idDem">
+								<input type="submit" name="idDem" value="<?php echo $array["idDem"] ?>" />
+							</td>
+							<td class="sujetDem">
+								<center>
+									<?php
+										echo $array["sujet"];
+									?>
+								</center>
+							</td>
+							<td class="mailDem">
+								<?php
+									echo $array["mailPers"];
+								?>
+						</tr>
+						<?php
+					}
+					?>
+				</form>
+			</table>
 			<?php
-			echo "<a href=\"?page=3\">Retour</a>";
+			}
 		}
 	}
 	else
 	{
-		$sql = $conn->prepare ("SELECT idDem, sujet, mailProf as mailPers, status FROM `demandes` WHERE status = 'Envoyee'");
-		$sql->execute(array());
-
-
-		if (is_null($sql) || $sql->rowCount() == 0)
-		{
-			echo "</br>Vous n'avez aucune demande en cours.";
-		}
-		else
-		{
-		?>
-		<table>
-			<tr class="presentation">
-				<td class="idDem">
-					<center>id :</center>
-				</td>
-				<td class="sujetDem">
-					<center>Sujet :</center>
-				</td>
-				<td class="mailDem">
-					<center>
-						<?php
-							if (isProfesseur())
-							{
-								echo "Etudiant :";
-							}
-							else
-							{
-								echo "Référent :";
-							}
-						?>
-					</center>
-				</td>
-			</tr>
-			<!-- Ecarte le nom des colonnes du contenu des colonnes resultat -->
-			<tr class="ecarteColonne"></tr>
-			<form method="post" action="projet.php?page=3">
-				<input type="hidden" name="checkForm" value="formulaire" /></br>
-				<?php
-				while($array = $sql->fetch(PDO::FETCH_ASSOC))
-				{
-					?>
-					<tr>
-						<td class="idDem">
-							<input type="submit" name="idDem" value="<?php echo $array["idDem"] ?>" />
-						</td>
-						<td class="sujetDem">
-							<center>
-								<?php
-									echo $array["sujet"];
-								?>
-							</center>
-						</td>
-						<td class="mailDem">
-							<?php
-								echo $array["mailPers"];
-							?>
-					</tr>
-					<?php
-				}
-				?>
-			</form>
-		</table>
-		<?php
-		}
+		header("refresh:0;url=projet.php");
+		die(0);
 	}
-}
-else
-{
-	header("refresh:0;url=projet.php");
-	die(0);
-}
 ?>
